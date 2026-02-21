799f577256d32bbab743d1f17f3e09ec346231e0
Ramon Villarroel <ramonjesusmb@gmail.com>
2026-02-21 08:36:31 -0300

    inicio del proyecto

---
+++ Control-de-stock-ef/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Control_de_stock_ef.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
